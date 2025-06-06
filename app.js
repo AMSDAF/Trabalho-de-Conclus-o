c84afdc864b2b485791f7288f41d8245a79de57a
Antonio Marcos <Antoniomarcossoaresdearaujo8@gmail.com>
2025-06-05 23:41:29 -0300

    TCC V04

---
+++ app.js
@@ -0,0 +1,17 @@
+const ferramentasButton = document.querySelector('.ferramentas-button')
+const ferramentasContent = document.querySelector('.ferramentas-content')
+const ferramentasSeta = document.querySelector('.seta-ferramentas')
+
+ferramentasButton.addEventListener('click', () => {
+    ferramentasContent.classList.toggle('active');
+    ferramentasSeta.classList.toggle('active');
+})
+
+const projetosButton = document.querySelector('.projetos-button')
+const projetosContent = document.querySelector('.projetos-content')
+const projetosSeta = document.querySelector('.seta-projetos')
+
+projetosButton.addEventListener('click', () => {
+    projetosContent.classList.toggle('active');
+    projetosSeta.classList.toggle('active')
+})
